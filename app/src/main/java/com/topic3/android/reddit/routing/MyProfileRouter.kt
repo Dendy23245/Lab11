19b36c694dce5e620c70b6e5b18e67d8fee74882
Dendy23245 <grifonchik1337@gmail.com>
2023-05-26 08:01:38 +0300

    change MyProfileRouter

--- app/src/main/java/com/topic3/android/reddit/routing/MyProfileRouter.kt
+++ app/src/main/java/com/topic3/android/reddit/routing/MyProfileRouter.kt
@@ -3,11 +3,6 @@ package com.topic3.android.reddit.routing
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
-/**
- * Класс, определяющий экраны, которые есть в нашем приложении.
- *
- * Эти объекты должны соответствовать файлам, которые есть в пакете screens
- */
 sealed class MyProfileScreenType {
   object Posts : MyProfileScreenType()
   object About : MyProfileScreenType()
